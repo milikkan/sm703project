d236d3682b980e2fb82996c99ac361d1a416f9cb
Mustafa ILIKKAN <milikkan.dev@gmail.com>
2020-06-02 14:15:40 +0300

    Add welcome message

--- src/main/java/sm703/Adder.java
+++ src/main/java/sm703/Adder.java
@@ -3,5 +3,6 @@ package sm703;
 public class Adder {
     public static void main(String[] args) {
         System.out.println("Hello...");
+        System.out.println("Welcome to Addatron. This is a sophisticated adder tool.");
     }
 }
