d6903598430803369091cc8d799a0e86897b0bd8
Mustafa ILIKKAN <milikkan.dev@gmail.com>
2020-06-06 11:25:50 +0300

    Reset progress

--- src/test/java/sm703/AdderTest.java
+++ src/test/java/sm703/AdderTest.java
@@ -5,9 +5,5 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AdderTest {
-    @Test
-    public void adds5And3Correctly() {
-        Adder testAdder = new Adder(5, 3);
-        assertEquals(8, testAdder.addNumbers());
-    }
+
 }
