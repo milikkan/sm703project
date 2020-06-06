f04e3efe55ed0d1520e8c0d2087a57b14aad3730
Mustafa ILIKKAN <milikkan.dev@gmail.com>
2020-06-06 15:59:26 +0300

    Complete addNumbers method for integer

--- src/main/java/sm703/Adder.java
+++ src/main/java/sm703/Adder.java
@@ -2,8 +2,14 @@ package sm703;
 
 public class Adder {
 
+    public int addNumbers(int num1, int num2) {
+        return num1 + num2;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello...");
         System.out.println("Welcome to Addatron. This is a sophisticated adder tool.");
+        Adder adder = new Adder();
+        System.out.println("Adding 3 and 5:" + adder.addNumbers(3, 5));
     }
 }
